49b88c368dfa61ea10408fc0c3980ac8e4dafea0
zatsando <zvracy13@gmail.com>
2021-12-06 13:32:07 +0200

    asdsad

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ const searchClient = algoliasearch(
   const search = instantsearch({
     searchFunction(helper) {
     const container = document.querySelector('#hits');
-    const container2 = document.querySelector('#sub-categories');
+    const container2 = document.querySelector('#categories-gr');
     const container3 = document.querySelector('#colors');
+    const container4 = document.querySelector('#occasion_gr');
     container.style.display = helper.state.query === '' ? 'none' : '';
     container2.style.display = helper.state.query === '' ? 'none' : '';
     container3.style.display = helper.state.query === '' ? 'none' : '';
+    container4.style.display = helper.state.query === '' ? 'none' : '';
     helper.search();
     },
     indexName: 'Test_01',
@@ -66,14 +68,14 @@ const searchClient = algoliasearch(
 
   search.addWidgets([
     instantsearch.widgets.hits({
-      container: "#sub-categories",
+      container: "#categories-gr",
       limit:3,
       attributeName: "subcategory_gr",
       templates:{
         empty: 'Δε βρέθηκε τίποτα για <q>{{ query }}</q>',
         item:`
         <div class="filter-widgets">
-          <div>{{subcategory_gr}}</div>
+          <div>{{subcategory_gr}} </div>
         </div>
         `
       }
@@ -83,71 +85,33 @@ const searchClient = algoliasearch(
   search.addWidgets([
     instantsearch.widgets.hits({
       container: "#colors",
-      limit:5,
+      limit:3,
       attributeName: "color_gr",
       templates:{
         empty: 'Δε βρέθηκε τίποτα για <q>{{ query }}</q>',
         item:`
         <div class="filter-widgets">
-          <div>{{color_gr}}</div>
+          <div>{{color_gr}} </div>
         </div>
         `
       }
     })
   ]);
 
-
-  //autocomplete
-  const autocomplete = instantsearch.connectors.connectAutocomplete(
-    ({ indices, refine, widgetParams }, isFirstRendering) => {
-      const { container, onSelectChange } = widgetParams;
-      if (isFirstRendering) {
-        container.html('<select id="ais-autocomplete"></select>');
-        container.find('select').selectize({
-          options: [],
-          valueField: 'query',
-          labelField: 'query',
-          highlight: false,
-          onType: refine,
-          onBlur() {
-            refine(this.getValue());
-          },
-          onChange(value) {
-            refine(value);
-            onSelectChange({
-              category: this.getOption(value).data('subcategory_gr'),
-              query: value,
-            });
-          },
-          score() {
-            return function() {
-              return 1;
-            };
-          },
-          render: {
-            option(item) {
-              // prettier-ignore
-              const [category] = item.instant_search.facets.exact_matches.categories;
-              return `
-                <div class="option" data-category="${category.value}">
-                  ${item.query} in <i>${category.value}</i>
-                </div>
-              `;
-            },
-          },
-        });
-  
-        return;
+  search.addWidgets([
+    instantsearch.widgets.hits({
+      container: "#occasion_gr",
+      limit:3,
+      attributeName: "occasion_gr",
+      templates:{
+        empty: 'Δε βρέθηκε τίποτα για <q>{{ query }}</q>',
+        item:`
+        <div class="filter-widgets">
+          <div>{{occasion_gr}} </div>
+        </div>
+        `
       }
-  
-      const [select] = container.find('select');
-  
-      select.selectize.clearOptions();
-      indices.forEach(index => {
-        index.results.hits.forEach(hit => select.selectize.addOption(hit));
-      });
-      select.selectize.refreshOptions(select.selectize.isOpen);
-    }
-  );
+    })
+  ]);
 
   search.start();
